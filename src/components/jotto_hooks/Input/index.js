9d85b74dc006fdab201836d506ddaf0116567de0
Robert Molina <rmolinamir@gmail.com>
2019-10-29 22:09:22 -0400

    input component `useState` testing & implementation

--- src/components/jotto_hooks/Input/index.js
+++ src/components/jotto_hooks/Input/index.js
@@ -3,8 +3,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Input(props) {
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
+  /**
+   * Handles input `onChange` event by setting the `currentGuess` value.
+   * @param {object} event - Input on change event.
+   */
+  function handleOnChange(event) {
+    event.preventDefault();
+    setCurrentGuess(event.target.value);
+  }
+
+  /**
+   * Handles submit button `onClick` event by clearing the `currentGuess` value.
+   * @param {object} event - Input on change event.
+   * TODO: Update `guessedWords` context.
+   * TODO: Check if `currentGuess` matches `secretWord`, and update `success` context
+   * if they are equal.
+   */
+  function handleOnClick(event) {
+    event.preventDefault();
+    setCurrentGuess('');
+  }
+
   return (
-    <div data-test="component-input" />
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="Enter guess"
+          value={currentGuess}
+          onChange={handleOnChange}
+        />
+        <button
+          data-test="submit-button"
+          onClick={handleOnClick}
+          className="btn btn-primary mb-2"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   )
 }
 
--- src/components/jotto_hooks/Input/index.test.js
+++ src/components/jotto_hooks/Input/index.test.js
@@ -16,8 +16,8 @@ const defaultProps = {
  * Setup function for app components
  * @returns {ShallowWrapper}
  */
-function setup() {
-  return shallow(<Input />);
+function setup(initialProps) {
+  return shallow(<Input {...initialProps} />);
 }
 
 it('renders without errors', () => {
@@ -28,4 +28,38 @@ it('renders without errors', () => {
 
 it('renders with the correct props', () => {
   checkPropTypes(Input, defaultProps);
+});
+
+describe('state controlled input field', () => {
+  let wrapper;
+  let setCurrentGuessMock;
+  beforeEach(() => {
+    // Setting up mock functions
+    setCurrentGuessMock = jest.fn();
+    React.useState = jest.fn(() => ['', setCurrentGuessMock]);
+    // Setting 
+    wrapper = setup();
+  });
+
+  afterEach(() => {
+    setCurrentGuessMock.mockClear();
+  });
+
+  it('state updates with value of input box upon change', () => {
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    // Simulating onChange event
+    const mockEvent = { target: { value: 'train' }, preventDefault() {} };
+    inputBox.simulate('change', mockEvent);
+    // Assertion
+    expect(setCurrentGuessMock).toHaveBeenCalledWith('train');
+  });
+
+  it('state updates with value of input box upon change', () => {
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    // Simulating onClick event
+    const mockEvent = { preventDefault() {} };
+    submitButton.simulate('click', mockEvent);
+    // Assertion
+    expect(setCurrentGuessMock).toHaveBeenCalledWith('');
+  });
 });
